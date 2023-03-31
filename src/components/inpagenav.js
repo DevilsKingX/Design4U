8dc29cec1d1e8ac3f81f17492835d15a0f8acfcf
DevilsKingX <85121306+DevilsKingX@users.noreply.github.com>
2023-03-31 23:31:45 +0530

    onTouch

--- src/components/inpagenav.js
+++ src/components/inpagenav.js
@@ -6,10 +6,10 @@ export default function InPageNav(props){
 
     return(
         <div className={styles.navigationMenu}>
-            <div className={styles.navivationCategory} onClick={()=>props.scrollTo('designs')}>Designs ↘</div>
-            <div className={styles.navivationCategory} onClick={()=>props.scrollTo('reviews')}>Review ↘</div>
-            <div className={styles.navivationCategory} onClick={()=>props.scrollTo('price')}>Prices ↘</div>
-            <div className={styles.navivationCategory} onClick={()=>props.scrollTo('team')}>Team ↘</div>
+            <div className={styles.navivationCategory} onClick={()=>props.scrollTo('designs')} onTouchStart={()=>props.scrollTo('designs')}>Designs ↘</div>
+            <div className={styles.navivationCategory} onClick={()=>props.scrollTo('reviews')} onTouchStart={()=>props.scrollTo('reviews')}>Review ↘</div>
+            <div className={styles.navivationCategory} onClick={()=>props.scrollTo('price')} onTouchStart={()=>props.scrollTo('price')}>Prices ↘</div>
+            <div className={styles.navivationCategory} onClick={()=>props.scrollTo('team')} onTouchStart={()=>props.scrollTo('team')}>Team ↘</div>
         </div>
     )
 }
